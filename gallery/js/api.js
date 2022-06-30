b33a274a9523e717829d2c96ad069d476f92395e
JaeWan CHOI <wkdudhksl@gamil.com>
2022-06-30 14:12:25 +0900

    FIX - gallery_list

--- gallery/js/api.js
+++ gallery/js/api.js
@@ -9,22 +9,35 @@ async function galleryView(){
         response_json = await response.json()
         users = response_json
 
-        const galleries = document.getElementsByClassName("galleriesbox-galleries")
+        const galleries = document.getElementsByClassName("gallery_list")
 
         for (let i = 0; i < users.length; i++){
-            const new_gallery = document.createElement("div")
-            new_gallery.setAttribute("class", "galleriesbox-galleries-gallery")
+            var new_gallery = document.createElement("div")
+            new_gallery.setAttribute("class", "gallery")
             galleries[0].append(new_gallery)
+
+            var new_gallery_content = document.createElement("div")
+            new_gallery_content.setAttribute("class", "content")
+            new_gallery.append(new_gallery_content)
+            
+            var new_text = document.createElement("div")
+            new_text.setAttribute("class", "text")
+            new_gallery_content.append(new_text)
+            
+            var new_image = document.createElement("img")
+            new_image.setAttribute("class", "image")
+            new_image.setAttribute("src", "https://m.7art7.com/web/product/big/202012/4dc33b6141e960cb7f820ea58172f5d5.jpg")
+            new_gallery_content.append(new_image)
 
             nickname = users[i].nickname
 
             const new_nickname = document.createElement("p")
             new_nickname.innerText = nickname
-            new_gallery.append(new_nickname)
+            new_text.append(new_nickname)
 
             const new_quantity = document.createElement("p")
             new_quantity.innerText = '전시된 작품 : ' + users[i].paintings_list.length
-            new_gallery.append(new_quantity)
+            new_text.append(new_quantity)
         }
     }
     else{
