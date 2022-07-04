6bc4a1cc703360e6a3f615f59bdebad448b0944b
Roshu2 <kyumin1020@gmail.com>
2022-07-04 14:21:11 +0900

    ADD - authentication.js

---
+++ index/js/authentication.js
@@ -0,0 +1,67 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+
+// 페이지를 다시 로딩 하면 벌어지는 일들!
+window.onload = () => {
+    const payload = JSON.parse(localStorage.getItem("payload"));
+    // 아직 access 토큰의 인가 유효시간이 남은 경우
+    if (payload.exp > (Date.now() / 1000)) {
+
+    } else {
+        // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
+        const requestRefreshToken = async (url) => {
+            const response = await fetch(url, {
+                headers: {
+                    Accept: "application/json",
+                    'Content-Type': 'application/json',
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "refresh": localStorage.getItem("farm_refresh_token")
+                })
+            }
+            );
+            return response.json();
+        };
+
+        // 다시 인증 받은 accessToken을 localStorage에 저장하자.
+        requestRefreshToken(`http://127.0.0.1:8000/user/api/token/refresh/`).then((data) => {
+            // 새롭게 발급 받은 accessToken을 localStorage에 저장
+            const accessToken = data.access;
+            localStorage.setItem("farm_access_token", accessToken);
+        });
+    }
+};
+
+function onLogout(){
+    localStorage.removeItem("farm_access_token")
+    localStorage.removeItem("farm_refresh_token")
+    localStorage.removeItem("payload")
+}
---
+++ mypage/js/mypage.js
@@ -0,0 +1,115 @@
+async function userGalleryView(){
+
+    const nickname = location.href.split('?')[1]
+    
+    paintingList = await getUserGalleryData()
+    
+    const userTitle = document.getElementsByClassName("user_title")
+    userTitle[0].innerText = nickname + "'s"+' GALLERY'
+
+    const galleries = document.getElementsByClassName("gallery_list")
+    
+    for (let i = 0; i < paintingList.length; i++){
+        
+        let painting = paintingList[i]
+        let title = painting.title
+        let artist = painting.artist.nickname
+
+        const newGallery = document.createElement("a")
+        newGallery.setAttribute("class", "gallery")
+        newGallery.addEventListener("click", () => {
+            modalView(painting)})
+        galleries[0].append(newGallery)
+        
+        const newGalleryContent = document.createElement("div")
+        newGalleryContent.setAttribute("class", "content")
+        newGallery.append(newGalleryContent)
+        
+        const newText = document.createElement("div")
+        newText.setAttribute("class", "text")
+        newGalleryContent.append(newText)
+        
+        const newImage = document.createElement("img")
+        newImage.setAttribute("class", "image")
+        newImage.setAttribute("src", "https://m.7art7.com/web/product/big/202012/a6352d03c87af7166e62c854ebc3b092.jpg")
+        newGalleryContent.append(newImage)
+        
+        const newTitle = document.createElement("p")
+        newTitle.innerText = title
+        newText.append(newTitle)
+        
+        const newArtist = document.createElement("p")
+        newArtist.innerText = artist
+        newText.append(newArtist)
+    }
+}
+
+async function loadMoreBtn(){
+    
+    const nickname = location.href.split('?')[1]
+
+    const users = await getUserGalleryData(nickname)
+
+    if (users.length > 8) {
+        const loadMoreBtn = document.getElementsByClassName("loadmore")
+        loadMoreBtn[0].style.display = 'block'
+    }
+}
+
+var galleriesCount = 8
+
+function loadData(){
+    
+    var galleries = document.querySelectorAll('.gallery')
+    
+    for (var i = galleriesCount; i < galleriesCount + 4; i++) {
+        if (galleries[i]) {
+            galleries[i].style.display = 'flex'
+        }
+    }
+    
+    galleriesCount += 4
+    
+    if (galleriesCount >= galleries.length) {
+        event.target.style.display = 'none'
+    }
+}
+
+function modalView(painting){
+
+    const body = document.getElementsByTagName('body')
+    const modal = document.getElementsByClassName('modal_body')
+    body[0].style.overflow = 'hidden'
+    modal[0].style.display = 'flex'
+
+    const image = document.getElementsByClassName('modal_image_box-image')
+    image[0].setAttribute('src', 'https://m.7art7.com/web/product/big/202012/a6352d03c87af7166e62c854ebc3b092.jpg')
+
+    const title = document.getElementsByClassName('modal_text_box-title')
+    const artist = document.getElementsByClassName('modal_text_box-artist')
+    const description = document.getElementsByClassName('modal_text_box-description')
+    const winningBid = document.getElementsByClassName('modal_text_box-winning_bid')
+    title[0].innerText = painting.title
+    artist[0].innerText = painting.artist.nickname
+    winningBid[0].innerText = `낙찰가 ${painting.auction[0].current_bid} point`
+    description[0].innerText = painting.description
+}
+
+function modalUnView(){
+    const body = document.getElementsByTagName('body')
+    const modal = document.getElementsByClassName('modal_body')
+    
+    modal[0].style.display = 'none'
+    body[0].style.overflow = 'auto'
+}
+
+const modalBody = document.querySelector('.modal_body')
+const modalBtn = document.querySelector('.modal_btn')
+modalBody.addEventListener('click', (e) => {
+    if (e.target == modalBody) {
+        modalUnView()
+    }
+})
+
+userGalleryView()
+loadMoreBtn()
