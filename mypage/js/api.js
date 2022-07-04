c619e0e88ce4cca548fe2a7fc8d2924c712ae5d3
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-04 22:07:38 +0900

    ADD - api.js

---
+++ mypage/js/api.js
@@ -0,0 +1,42 @@
+const backendBaseUrl = "http://127.0.0.1:8000"
+const frontendBaseUrl = "http://127.0.0.1:5500"
+const token = localStorage.getItem("farm_access_token");
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+
+async function getMyPageData(nickname){
+    
+    const response = await fetch(`${backendBaseUrl}/mypage/${nickname}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-CSRFToken': csrftoken,
+            'Authorization': 'Bearer ' + token,
+        }
+    })
+    
+    if (response.status == 200){
+        data = await response.json()
+        return data
+    }
+    else {
+        console.log(response.status, "유저 활동 데이터가 없습니다")
+        return response.status
+    }
+}
