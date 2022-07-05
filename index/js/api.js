61e962368d7620725840119f56fe88ff69f75ec1
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-05 23:47:18 +0900

    FIX - Clean Code

--- index/js/api.js
+++ index/js/api.js
@@ -2,64 +2,6 @@ const backendBaseUrl = "http://127.0.0.1:8000"
 const frontendBaseUrl = "http://127.0.0.1:5500"
 const token = localStorage.getItem("farm_access_token");
 
-<<<<<<< HEAD
-window.onload = () => {
-    const payload = JSON.parse(localStorage.getItem("payload"));
-    // 아직 access 토큰의 인가 유효시간이 남은 경우
-    if (payload.exp > (Date.now() / 1000)) {
-
-    } else {
-        // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
-        const requestRefreshToken = async (url) => {
-            const response = await fetch(url, {
-                mode: 'cors',
-                headers: {
-                    Accept:"application/json",
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrftoken,
-                    'Authorization': 'Bearer ' + token,
-                },
-                method: "POST",
-                body: JSON.stringify({
-                    "refresh": localStorage.getItem("farm_refresh_token")
-                })
-            }
-            );
-            return response.json();
-        };
-
-        // 다시 인증 받은 accessToken을 localStorage에 저장하자.
-        requestRefreshToken(`${backendBaseUrl}/user/api/token/refresh/`).then((data) => {
-            // 새롭게 발급 받은 accessToken을 localStorage에 저장
-            const accessToken = data.access;
-
-            localStorage.setItem("farm_access_token", accessToken);
-        });
-    }
-};
-=======
->>>>>>> detail
-
-// function styleTransform() {
-//     const originalImg = document.getElementById('image-upload').style.backgroundImage;
-//     $.ajax({
-//         type: 'POST',
-//         url: `${backend_base_url}/main`,
-//         data: {originalImg},
-//         headers: { 'Authorization': localStorage.getItem("token") }, 
-//         success: function (response) {
-//             let image = response['image']
-//             console.log(image)
-
-//             $('#result-img').style.backgroundImage=img
-//         }
-//     })
-//     console.log(formData);
-//     console.log("로딩 시작");
-//     const response_json = await response.json();
-//     console.log("로딩 끝");
-//     console.log(originalImg);
-// }
 
 async function uploadAuction() {
     const category = document.getElementById('category').value;
