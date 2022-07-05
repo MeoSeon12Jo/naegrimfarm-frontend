59d58fb4cb801a896f70d0672b7153e3808f34c4
JaeWan CHOI <wkdudhksl@gamil.com>
2022-07-05 23:47:57 +0900

    ADD - function Reader

--- index/js/mainpage.js
+++ index/js/mainpage.js
@@ -1,8 +1,25 @@
-function goMyPage(){
+function goMyPage() {
 
     const nickname = JSON.parse(localStorage.getItem("payload"))['nickname']
     const myPage = document.getElementsByClassName("my-page")
     myPage[0].setAttribute("href", `/mypage/mypage.html?${nickname}`)
+}
+
+function Reader(event) {
+    const columnDiv1 = document.getElementsByClassName('column-div1')
+    const inputBox = document.getElementsByClassName('inputBox')
+    columnDiv1[0].style.border = 'none'
+    inputBox[0].style.display = 'none'
+    var data = event.target
+    var reader = new FileReader()
+    reader.onload = function () {
+        var dataURL = reader.result
+        var offset = "previewImage"
+        var output = document.getElementsByClassName(offset)
+        output[0].style.display = 'block'
+        output[0].src = dataURL
+    }
+    reader.readAsDataURL(data.files[0])
 }
 
 //네비바 유저 포인트
@@ -13,7 +30,7 @@ function goMyPage(){
 //     let userPoint = auctions['user_point']
 
 //     console.log(userPoint)
-  
+
 //     const userCurrentPoint = document.getElementsByClassName("user-point")[0]
 
 //     userCurrentPoint.replaceChildren()
@@ -22,5 +39,4 @@ function goMyPage(){
 //     newUserPoint.setAttribute("class", "point-int")
 //     newUserPoint.innerText = "POINT " + userPoint.toLocaleString()
 //     userCurrentPoint.append(newUserPoint)
-
 // }
