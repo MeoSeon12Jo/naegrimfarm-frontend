17bfb7fac190187c4d6523c4f6b9fb70c4d9ecbf
Roshu2 <kyumin1020@gmail.com>
2022-07-06 01:33:30 +0900

    .

--- mypage/js/api.js
+++ mypage/js/api.js
@@ -2,22 +2,6 @@ const backendBaseUrl = "http://3.39.193.83"
 const frontendBaseUrl = "https://neon-queijadas-c23bce.netlify.app"
 const token = localStorage.getItem("farm_access_token");
 
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-var csrftoken = getCookie('csrftoken');
 
 
 async function getMyPageData(nickname){
